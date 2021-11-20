60592fe38c731d6fe0e665f58ec04df408a6c227
Jean Barbosa <programmer.jean@gmail.com>
2021-11-20 16:41:33 -0300

    feat: desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,13 @@
-export function Content() {
-  // Complete aqui
+interface ContentProps {
+  selectedGenre: {
+    title: string;
+  }
+}
+
+export function Content({ selectedGenre }: ContentProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+  )
 }
